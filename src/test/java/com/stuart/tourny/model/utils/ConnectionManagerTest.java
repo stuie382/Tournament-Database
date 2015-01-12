9c8ae052df469c61535913a3d0e4a7247b29886c
stuie382 <stu382@hotmail.co.uk>
2015-01-12 20:45:06 +0000

    Tidy up connection manager test

--- src/test/java/com/stuart/tourny/model/utils/ConnectionManagerTest.java
+++ src/test/java/com/stuart/tourny/model/utils/ConnectionManagerTest.java
@@ -20,30 +20,6 @@ public class ConnectionManagerTest extends TestCase {
 
     }
 
-    /*
-        public Test () {
-            try {
-                init ();
-                System.out.println (tester (ConnectionManager.getInstance ().getConnection ()));
-                System.out.println (insertTest (ConnectionManager.getInstance ().getConnection ()));
-                System.out.println (getInsertTest (ConnectionManager.getInstance ().getConnection
-                 ()));
-            } catch (Exception ex) {
-                System.out.println ("All gone wrong: " + ex.getMessage ());
-            }
-        }
-
-        public List<String> tester (Connection con) throws SQLException {
-            List<String> result = new ArrayList<> ();
-            Statement s = con.createStatement ();
-            ResultSet rs = s.executeQuery ("select * from app.PLAYER");
-            while (rs.next ()) {
-                result.add (rs.getString (1));
-            }
-            con.close ();
-            return result;
-        }
-    */
     @Test
     public void testInsertTest () throws Exception {
 
@@ -58,20 +34,4 @@ public class ConnectionManagerTest extends TestCase {
         }
         assertEquals (1, updated);
     }
-/*
-    public String getInsertTest (Connection con) {
-        String sql = "select * from app.player where name = 'bob'";
-        String result = "";
-        try {
-            Statement s = con.createStatement ();
-            ResultSet rs = s.executeQuery (sql);
-            if (rs.next ()) {
-                result = rs.getString (1);
-            }
-            con.close ();
-        } catch (Exception ex) {
-            System.out.println ("getInsertTest fail: " + ex.getMessage ());
-        }
-        return result;
-    }*/
 }
