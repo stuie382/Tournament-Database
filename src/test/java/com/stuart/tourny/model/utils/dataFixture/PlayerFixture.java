af9001f9c085bc4e7a421b365fabac9fa2e57fd4
stuie382 <stu382@hotmail.co.uk>
2015-03-12 17:35:32 +0000

    Player fixture git issue fix.

---
+++ src/test/java/com/stuart/tourny/model/utils/dataFixture/PlayerFixture.java
@@ -0,0 +1,37 @@
+package com.stuart.tourny.model.utils.dataFixture;
+
+import com.stuart.tourny.model.common.dto.DTOPlayer;
+import com.stuart.tourny.model.engines.PlayerDbEngine;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class PlayerFixture {
+
+  private PlayerDbEngine playerEngine;
+
+  public PlayerFixture() {
+    playerEngine = new PlayerDbEngine();
+  }
+
+  /**
+   * Add a simple {@code Player} record with the given {@code name} attribute.
+   *
+   * @param conn
+   *     - Connection to the database
+   * @param name
+   *     - name to set on the record
+   *
+   * @return - Populated {@code DTOPlayer} record from the database
+   *
+   * @throws java.sql.SQLException
+   */
+  public DTOPlayer addAPlayer(Connection conn,
+                              String name) throws SQLException {
+    DTOPlayer dto = new DTOPlayer();
+    dto.setName(name);
+    dto = playerEngine.addPlayer(conn, dto);
+    return dto;
+  }
+
+}
